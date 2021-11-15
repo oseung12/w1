fc7a1938b0fece625f161a79a4cb406ce53c75fe
oseung12 <osungg@gmail.com>
2021-11-15 13:36:32 +0900

    modify

--- js/LottoGen01.js
+++ js/LottoGen01.js
@@ -1,29 +1,32 @@
+
+//1. 45개 번호 생성
+
+let lottnos = []
+let drawnos = []
+for (let i = 0; i < 45 ; i++) {
+    lottnos[i] = i +1
+}
+
+console.log(lottnos)
+
+//2. 숫자를 랜덤하게 추첨하여 추첨된 인덱스 번호를 splice를 이용해서 삭제
 //3. 삭제된 배열에서 새로운 번호 추첨
 //4. 번호 6개 추첨하여 배열 전달
 
-//1. 45개 번호 생성
-let LottoNos = []
-let DrawNos = []
-for (let i = 0; i < 45 ; i++) {
-    LottoNos[i] = i +1
-}
-
-console.log(LottoNos)
-//2. 숫자를 랜덤하게 추첨하여 추첨된 인덱스 번호를 splice를 이용해서 삭제
-
 for ( i = 0; i < 6; i++) {
-    let index = parseInt(Math.random() * LottoNos.length)
+    let index = parseInt(Math.random() * lottnos.length)
 
-    const num = LottoNos[index]
+    const num = lottnos[index]
     console.log(num)
 
-    if(DrawNos.indexOf(num) >= 0){
+    if(drawnos.indexOf(num) >= 0){
         console.log("중복 발생")
         i--
         continue
     }
 
-    DrawNos.push(num)
+    drawnos.push(num)
 }
 
-console.log(DrawNos)
+console.log(drawnos)
+
--- js/lotto.js
+++ js/lotto.js
@@ -1,70 +1,68 @@
+// console.log("lotto...................")
+//
+// const btn = document.getElementById("btn") //발생 주체
+//
+// console.log(btn)
+//
+// //function doA() {}
+// // btn.addEventListener("click", doA이 방법은 윈도우로 들어가므로 추천하지 않음.
+//
+// //함수자체 선언하는 방법
+// //btn.addEventListener("click",function(){})
 
-console.log("lotto................")
+// 화살표 함수 사용
+// btn.addEventListener("click",(event) => {
+//     console.log(event)
+//
+//     const target = document.getElementById("resultDiv")      //target 에 "h3"을 넣는다.
+//
+//     const balls = makeLottoNum()        //배열 즉, 루프를 돌릴수있다
+//
+//     //clear all <h3> tags
+//     target.innerHTML =''                //기존에 있었던 것은 모두 삭제 // 한꺼번에 DOM 작업을 한번만 하도록 만들어 주기.
+//
+//     let str =''
+//
+//     for (const num of balls) {
+//
+//         str += `<h3>${num}</h3>`
+//
+//         target.innerHTML = str
+//         // const tag = document.createElement("h3")
+//         // const textNode = document.createTextNode(num)
+//         // tag.appendChild(textNode)
+//         // target.appendChild(tag)
+//     }
+//
+//
+// }, false)    // 무조건 뒤에 false 작성.
 
-const btn = document.getElementById("btn")
+// (상단) 작성되어있는 부분은 이벤트발생의 표준이다.
 
+//(하단) 함수를 통한 로또 발생
+function makeLottoNum(){
 
-console.log(btn)
+    const arr = []
 
-
-//function doA() {}
-// btn.addEventListener("click", doA이 방법은 윈도우로 들어가므로 추천하지 않음.
-
-//함수자체 선언하는 방법
-//btn.addEventListener("click",function(){})
-
-//. 화살표함수사용
-btn.addEventListener("click",(event) => {
-    console.log(event)
-
-    const target = document.getElementById("resultDiv") //target에 "h3"을 넣는다.
-
-
-    const balls = makeLottoNum()   //배열 즉, 루프를 돌릴수있다
-
-    //clear all <h3> tags
-    target.innerHTML =''
-
-    for (const num of balls) {
-        const tag = document.createElement("h3")
-        const textNode = document.createTextNode(num)
-        tag.appendChild(textNode)
-        target.appendChild(tag)
-    }
-
-
-}, false) // 무조건 뒤에 false 작성.
-
-//여기까지 작성되어있는 부분은 이벤트발생의 표준이다.
-
-//하단부터는 함수를 통한 로또 발생
-function makeLottoNum() {
-
-    let LottoNos = []
-    let DrawNos = []
-    for (let i = 0; i < 45; i++) {
-        LottoNos[i] = i + 1
-    }
-
-    console.log(LottoNos)
-//2. 숫자를 랜덤하게 추첨하여 추첨된 인덱스 번호를 splice를 이용해서 삭제
-
-    for (i = 0; i < 6; i++) {
-        let index = parseInt(Math.random() * LottoNos.length)
-
-        const num = LottoNos[index]
-        console.log(num)
-
-        if (DrawNos.indexOf(num) >= 0) {
-            console.log("중복 발생")
+    for (let i = 0; i < 6; i++) {
+        const tempNum = parseInt(Math.random() * 45) + 1
+        //arr안에 tempNum이 있다면
+        if( arr.indexOf(tempNum) >= 0 ){
+            console.log("중복")
             i--
             continue
         }
-
-        DrawNos.push(num)
-
-
-
+        arr.push(tempNum)
     }
-    return DrawNos
+
+    // arr.sort(function(a,b) {
+    //     return a -b
+    // })
+
+    arr.sort((a,b) => a - b )
+
+    return arr
+
 }
+
+console.log(makeLottoNum())
